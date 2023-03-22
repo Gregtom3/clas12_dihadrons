2fc060bd09d5736d408b049860c88402d09a6965
Gregtom3 <gamatousek@gmail.com>
2023-03-22 09:31:20 -0400

    Machine learning code

---
+++ machine_learning/tools/handler.py
@@ -0,0 +1,93 @@
+import os
+import pickle
+import numpy as np
+from catboost import CatBoostClassifier
+from xgboost.sklearn import XGBClassifier
+
+
+#This function creates directories in the given output directory.
+def create_dirs(outdir="",
+                nn_type="",
+                models=[]):
+    
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+        print("Making directory",outdir)
+    
+    outdir=outdir+"/"+nn_type
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+        print("Making directory",outdir)
+    
+    for model in models:
+        model_type = model[1]
+        subdir=outdir+"/"+model_type
+        if not os.path.exists(subdir):
+            os.mkdir(subdir)
+            os.mkdir(subdir+"/eval_plots")
+            print("\tMaking subdirectory",subdir)
+            print("\t\tMaking subdirectory",subdir+"/eval_plots")
+    print("\n")
+    
+    return outdir
+
+# This function imports a model from a given directory
+# based on the model type and suffix
+
+# Define function parameters
+# savedir --> Directory where model is stored
+# model_type --> Type of model (gbt/xgb)
+# suffix --> One of the headers in utils/subdata.json
+def import_model(savedir="",
+                 model_type="",
+                 suffix=""):
+    
+    # Check if model type is GBT
+    if(model_type=="gbt"):
+        
+        # Create a CatBoostClassifier object
+        model = CatBoostClassifier()
+        
+        # Load model from given directory
+        model.load_model(f"{savedir}/gbt_model_{suffix}")
+    
+    # Check if model type is XGBoost
+    elif(model_type=="xgb"):
+        
+        # Load model from given directory
+        model = pickle.load(open(f"{savedir}/xgb_model_{suffix}.pkl", "rb"))
+        
+    # Check if model type is Random Forest
+    elif(model_type=="rf"):
+        
+        # Load model from given directory
+        model = pickle.load(open(f"{savedir}/rf_model_{suffix}.pkl", "rb"))
+    
+    # If model type is unknown
+    else:
+        
+        # Print a message
+        print("Unknown model type",model_type,"...returning -1...")
+        
+        # Return -1
+        return -1
+    
+    # Return model
+    return model
+
+
+#This function makes predictions with a given model
+#It takes three parameters:
+#model - the model that is used for making the predictions
+#model_type - the type of model used for making the predictions (can be either "gbt" or "xgb")
+#x - the data to be predicted on
+
+def make_predictions(model=0,
+                     model_type="",
+                     x=[]):
+    
+    if(model_type=="gbt" or model_type=="xgb" or model_type=="rf"):
+        return np.array(model.predict_proba(x)[:,1],dtype=float)
+    else:
+        print("Unknown model type",model_type,"...returning -1...")
+        return -1
---
+++ machine_learning/train.py
@@ -0,0 +1,105 @@
+from tools.dataloader import *
+from tools.handler import *
+from tools.plotter import *
+from random_forest.train import train as rf_train
+from gbt.train import train as gbt_train
+from xgb.train import train as xgb_train
+
+# ML imports
+# from catboost import CatBoostClassifier, Pool, metrics, cv
+# from sklearn.metrics import accuracy_score
+# from xgboost.sklearn import XGBClassifier
+# from sklearn.metrics import roc_curve, auc, confusion_matrix
+
+
+def train(rootdir = "/volatile/clas12/users/gmat/clas12analysis.sidis.data/clas12_dihadrons/projects/ana_v1/data/raw/pi0_pi0",
+          SUBDATA="MC_RGA_inbending",
+          yamlfile = "params.yaml",
+          nn_type  = "track", # calo or track (use either calorimeter info or track info to determine nearest neighbors)
+          outdir   = "./test"):
+    
+    
+    # Load the parameters from the yamlfile
+    yamlfile = "params.yaml"
+    models = load_params(yamlfile)
+    
+    # Create the output directories to store the models
+    outdir=create_dirs(outdir = outdir,
+                       nn_type = nn_type,
+                       models = models)
+    
+    # Load the rootfiles for the learning
+    # SUBDATA --> see utils/subdata.json
+    #         --> Must be one of the headers (ex: MC_RGA_inbending)
+    rootfiles = load_files(rootdir=rootdir,
+                           SUBDATA=SUBDATA,
+                           test=1)
+    
+    # Split the data into a training and validation set
+    if(nn_type=="calo"):
+        ttree="MLInput_calo"
+    elif(nn_type=="track"):
+        ttree="MLInput_track"
+    else:
+        print("Unknown nn_type",nn_type,"...defaulting to MLInput_calo...")
+        ttree="MLInput_calo"
+    train_pool, validation_pool = load_data(rootfiles = rootfiles,
+                                            ttree     = ttree,
+                                            version="train",
+                                            split_ratio = 0.75,
+                                            random_seed = 42)
+    
+    # For each of the models in the yamlfile, perform the training
+    for model in models:
+        model_name = model[0]
+        model_type = model[1]
+        model_params = model[2]
+        
+        # Define savedir
+        savedir=outdir+"/"+model_type
+        
+        print("\n\nProcessing model",model_name,"with type",model_type,"\n\n")
+        
+        # Determine which architecture to train with
+        # The model is saved to "savedir" which can be imported later
+        if(model_type=="gbt"):
+            gbt_train(train_pool,
+                      validation_pool,
+                      model_params,
+                      savedir,
+                      SUBDATA)
+        
+        elif(model_type=="xgb"):
+            xgb_train(train_pool,
+                      validation_pool,
+                      model_params,
+                      savedir,
+                      SUBDATA)
+        elif(model_type=="rf"):
+            rf_train(train_pool,
+                     validation_pool,
+                     model_params,
+                     savedir,
+                     SUBDATA)
+        else:
+            print("Unknown model type:",model_type,"...skipping...")
+            continue
+        
+        # Write the model parameters to a text file in the dictionary
+        with open(savedir+"/model_params.txt", "w") as f:
+            for key, value in model_params.items():
+                f.write(key + ": " + str(value) + "\n")
+
+        # Import the model and perform predictions with the validation set
+        trained_model = import_model(savedir,model_type, SUBDATA)
+        
+        # Make predictions from the model
+        X_validation = validation_pool[0]
+        y_validation = validation_pool[1]
+        predictions = make_predictions(trained_model,model_type,X_validation)
+        
+        # Create plots and save them to the model directories
+        make_plots(X_validation,y_validation,predictions,savedir,SUBDATA)
+        
+if __name__ == "__main__":
+    train()
