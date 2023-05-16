a53be27a3de434e8680693db9004c71ae69b1f1f
Gregtom3 <gamatousek@gmail.com>
2023-05-16 10:27:29 -0400

    Saving MC File gen

--- macros/hipoLUND2tree.C
+++ macros/hipoLUND2tree.C
@@ -2,6 +2,12 @@
 #include "../src/Structs.h"
 #include "../src/Kinematics.C"
 
+double angle(part p1, part p2){
+    TVector3 v1(p1.px,p1.py,p1.pz);
+    TVector3 v2(p2.px,p2.py,p2.pz);
+    return v1.Angle(v2)*180/3.14159265;
+}
+
 void generate_combinations(std::vector<int>& input, int num, int start_idx, std::vector<int>& curr_combination, std::vector<std::vector<int>>& result) {
     if (num == 0) {
         result.push_back(curr_combination);
@@ -57,21 +63,22 @@ std::vector<std::vector<int>> remove_duplicates(std::vector<std::vector<int>> in
 // For each event, the scattered electron is found
 // Then, the dihadrons are formed
 // The important event variables are then saved to a TTree
-int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/montecarlo/clasdis/fall2018/torus-1/v1/bkg45nA_10604MeV/45nA_job_3051_0.hipo",
+int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/montecarlo/clasdis/fall2018/torus+1/v1/bkg50nA_10604MeV/50nA_OB_job_3313_0.hipo",
                   const char * outputFile = "hipoLUND2tree.root",
                   const int pid_h1=211,
                   const int pid_h2=-211,
-                  const int maxEvents=50000000){
+                  const int maxEvents=500000000){
     
   // Open TTree and declare branches
   // -------------------------------------
   TFile *fOut = new TFile(outputFile,"RECREATE");
-  TTree* tree = new TTree("dihadron","dihadron");
-  double _electron_beam_energy;
-  int run;
-  double x,y,z,nu,s,Q2,W,xF1,xF2,xF,P1,P2,z1,z2,Mh,Mx,pT1,pT2,pTtot,phi_h,phi_R0,phi_R1,th;
+  TTree* tree = new TTree("dihadron_cuts","dihadron_cuts");
+  double _electron_beam_energy = 10.6041;
+  int run,passDihadron,evnum;
+  double x,y,z,nu,s,Q2,W,xF1,xF2,xF,P1,P2,z1,z2,Mh,Mx,pT1,pT2,pTtot,phi_h,phi_R0,phi_R1,th,E_e,theta_e,phi_e;
     
-
+  tree->Branch("passDihadron",&passDihadron,"passDihadron/I");
+  tree->Branch("evnum", &evnum, "evnum/I");
   tree->Branch("x", &x, "x/D");
   tree->Branch("y", &y, "y/D");
   tree->Branch("z", &z, "z/D");
@@ -93,7 +100,20 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
   tree->Branch("phi_R0", &phi_R0, "phi_R0/D");
   tree->Branch("phi_R1", &phi_R1, "phi_R1/D");
   tree->Branch("th", &th, "th/D");
+
     
+  TTree* dis_tree = new TTree("DIS","DIS");
+  dis_tree->Branch("x", &x, "x/D");
+  dis_tree->Branch("y", &y, "y/D");
+  dis_tree->Branch("z", &z, "z/D");
+  dis_tree->Branch("nu", &nu, "nu/D");
+  dis_tree->Branch("s", &s, "s/D");
+  dis_tree->Branch("Q2", &Q2, "Q2/D");
+  dis_tree->Branch("W", &W, "W/D"); 
+  dis_tree->Branch("E_e", &E_e, "E_e/D");
+  dis_tree->Branch("theta_e", &theta_e, "theta_e/D");    
+  dis_tree->Branch("phi_e", &phi_e, "phi_e/D");
+  
   // Configure CLAS12 Reader and HipoChain
   // -------------------------------------
   clas12root::HipoChain _chain;
@@ -101,11 +121,12 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
 
   _chain.Add(input_file);
   _config_c12=_chain.GetC12Reader();  
-    
+
   // Add RUN::config bank
   // -------------------------------------
   int _idx_RUNconfig = _config_c12->addBank("RUN::config");
   int _irun = _config_c12->getBankOrder(_idx_RUNconfig,"run");
+  int _ievnum = _config_c12->getBankOrder(_idx_RUNconfig,"event");
   
   // Establish CLAS12 event parser
   // -------------------------------------
@@ -126,6 +147,7 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
   // Declare TLorentzVectors
   // -------------------------------------
   TLorentzVector vec_eIn, vec_eOut,q,h1,h2,dihadron;
+  vec_eIn.SetPxPyPzE(0,0,sqrt(_electron_beam_energy*_electron_beam_energy-Me*Me),_electron_beam_energy);
   TLorentzVector init_target(0,0,0,Mp);  
     
   // Declare indecies for tracking dihadron particles
@@ -142,16 +164,20 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
     if(whileidx%10000==0 && whileidx!=0){
       std::cout << whileidx << " events read" << std::endl;
     }
+    evnum = whileidx;
     whileidx++;
-      
+
     auto event = _c12->event();
-    
+
     // Clear vectors
     mcparts.clear();
     h1_idxs.clear();
     h2_idxs.clear();
     dihadron_idxs.clear();
-      
+    
+    // Scattered estruct
+    part eOut_struct;
+    
     // Get run specific information
     // -------------------------------------
     run = _c12->getBank(_idx_RUNconfig)->getInt(_irun,0);
@@ -178,10 +204,6 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
       partstruct.pt = _kin.Pt(partstruct.px,partstruct.py);
       partstruct.p  = _kin.P(partstruct.px,partstruct.py,partstruct.pz);
       partstruct.E  = _kin.E(partstruct.m,partstruct.p);
-      if(idx==0) {
-          vec_eIn.SetPxPyPzE(0,0,partstruct.pz,partstruct.E);
-          _electron_beam_energy=partstruct.E;
-      }
       
       partstruct.theta = _kin.th(partstruct.pt,partstruct.pz);
       partstruct.eta = _kin.eta(partstruct.theta);
@@ -198,6 +220,7 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
         W=_kin.W(Q2,Mp,nu);
         x=_kin.x(Q2,s,y);
         partstruct.is_scattered_electron=1;
+        eOut_struct=partstruct;
         vec_eOut.SetPxPyPzE(partstruct.px,partstruct.py,partstruct.pz,partstruct.E);
       }
         
@@ -209,9 +232,33 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
     }
     
 
-      
+
     q=vec_eIn-vec_eOut;
     
+    E_e = vec_eOut.E();
+    theta_e = vec_eOut.Theta();
+    phi_e = vec_eOut.Phi();
+      
+    // Reject events that fail DIS & electron cuts
+    if(vec_eOut.Theta()*180/3.14159265<5||vec_eOut.Theta()*180/3.14159265>35) continue;
+    if(y>0.8) continue;
+    if(W<2) continue;
+    if(Q2<1) continue;
+    // Fill DIS tree despite dihadron cuts not passing
+    dis_tree->Fill();
+    
+    // Make sure there are enough hadrons in the event to produce dihadrons
+    int num_h1=0;
+    int num_h2=0;
+    for(part particle : mcparts){
+      if(particle.pid==fs.pid_h1) num_h1++;
+      else if(particle.pid==fs.pid_h2) num_h2++;
+    }
+
+    if(num_h1 < fs.num_h1 || num_h2 < fs.num_h2) {
+      continue; // skip event if it won't produce any dihadrons for us
+    }
+      
     int Nmax = mcparts.size();
     //Loop over all particles in the event to determine hadron indecies
 
@@ -219,6 +266,8 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
             if(mcparts[i].pid==pid_h1 || (mcparts[i].pid==22 && pid_h1==111)) h1_idxs.push_back(i);
             if(mcparts[i].pid==pid_h2 || (mcparts[i].pid==22 && pid_h2==111)) h2_idxs.push_back(i);
         }
+      
+      
         //Now form all possible dihadron index pairs
         if(pid_h1==pid_h2 && pid_h1!=111){dihadron_idxs=unique_combinations(h1_idxs,2); }
         else if(pid_h1==pid_h2 && pid_h1==111){dihadron_idxs=unique_combinations(h1_idxs,4);}
@@ -250,9 +299,10 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
                 }
             }
         }
-    
+
         // Remove any instance of duplicate dihadrons
         dihadron_idxs = remove_duplicates(dihadron_idxs);
+
         // Now loop over all dihadrons
         for(int a = 0 ; a < dihadron_idxs.size() ; a++){
             std::vector<int> dihadron_idx = dihadron_idxs.at(a);
@@ -321,19 +371,55 @@ int hipoLUND2tree(const char * input_file = "/cache/clas12/rg-a/production/monte
 
             P1=h1.P();
             P2=h2.P();
-            if(y>0.8) continue;
-            if(W<2) continue;
-            if(Q2<1) continue;
-            if(pid_h1!=111&&P1<1.25) continue;
-            if(pid_h2!=111&&P2<1.25) continue;
+            
+ 
+            
+            // passDihadron == false for events that fail our dihadron cuts
+            passDihadron=true;
+            
+            // ---> Minimum momentum cuts for hadrons
+            if(pid_h1!=111&&P1<1.25) passDihadron=false;
+            if(pid_h2!=111&&P2<1.25) passDihadron=false;
+            
+            // ---> Missing mass cut for pi+pi- and pi+pi0 final states
             if((pid_h1==211&&pid_h2==-211)||(pid_h1==211&&pid_h2==111)){
-                if(Mx<1.5) continue;
+                if(Mx<1.5) passDihadron=false;
             }
-            if(z>0.95) continue;
-            if(xF1<0||xF2<0) continue;
-            tree->Fill();
+            
+            // ---> Maximum z to avoid exclusive events
+            if(z>0.95) passDihadron=false;
+            
+            // ---> CFR hadrons
+            if(xF1<0||xF2<0) passDihadron=false;
+            
+            // ---> Minimum photon energy
+            if(pid_h1==111&&(mcparts[i].E<0.2||mcparts[ii].E<0.2)) passDihadron=false;
+            if(pid_h2==111&&(mcparts[j].E<0.2||mcparts[jj].E<0.2)) passDihadron=false;
+            
+            // ---> Minimum angle between scattered e- and gamma, and FD cut on photons
+            if(pid_h1==111){
+                if(angle(eOut_struct,mcparts[i])<8||angle(eOut_struct,mcparts[ii])<8) passDihadron=false;
+                if(mcparts[i].theta*180/3.141592<5||mcparts[i].theta*180/3.14159265>35) passDihadron=false;
+                if(mcparts[ii].theta*180/3.141592<5||mcparts[ii].theta*180/3.14159265>35) passDihadron=false;
+            }
+            if(pid_h2==111){
+                if(angle(eOut_struct,mcparts[j])<8||angle(eOut_struct,mcparts[jj])<8) passDihadron=false;
+                if(mcparts[j].theta*180/3.141592<5||mcparts[j].theta*180/3.14159265>35) passDihadron=false;
+                if(mcparts[jj].theta*180/3.141592<5||mcparts[jj].theta*180/3.14159265>35) passDihadron=false;
+            }
+            
+            // ---> FD cut on hadrons
+            if(pid_h1!=111 &&(h1.Theta()*180/3.14159265<5||h1.Theta()*180/3.14159265>35)) passDihadron=false;
+            if(pid_h2!=111 &&(h2.Theta()*180/3.14159265<5||h2.Theta()*180/3.14159265>35)) passDihadron=false;
+            
+            if(passDihadron)
+                tree->Fill();
+            
+            // Fill DIS tree despite dihadron cuts not passing
+            dis_tree->Fill();
         } // end dihadron loop
     }// end event loop
+    dis_tree->Write();
     tree->Write();
     fOut->Close();
     return 0;
